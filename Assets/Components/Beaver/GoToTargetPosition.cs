ff24f3963e0fcc7036e685566d1ff623c9ad6419
Denis Dvorkovyy <prutmaild@gmail.com>
2024-11-18 16:48:47 +0100

    Started work on beavor building damm against wall behaviour

--- Assets/Components/Beaver/GoToTargetPosition.cs
+++ Assets/Components/Beaver/GoToTargetPosition.cs
@@ -1,19 +1,19 @@
-using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
 
-public class GoToTargetTransform : Action
+public class GoToTargetPosition : Action
 {
     public SharedFloat speed = 0;
-    public SharedTransform target;
+    public SharedVector3 target;
     public override TaskStatus OnUpdate()
     {
         if (Vector3.SqrMagnitude(transform.position -
-                                 target.Value.position) < 0.1f) {
+                                 target.Value) < 0.1f) {
             return TaskStatus.Success;
         }
         transform.position = Vector3.MoveTowards(transform.position,
-                                                 target.Value.position, speed.Value * Time.deltaTime);
+                                                 target.Value, speed.Value * Time.deltaTime);
         transform.LookAt(target.Value);
         return TaskStatus.Running;
     }
